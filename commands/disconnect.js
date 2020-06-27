24fb13135b1331a17e6dddae53499c5d323815b8
ArtuKILL <39144443+ArtuKILL@users.noreply.github.com>
2020-06-27 14:47:35 -0400

    Arreglado exepciones de los comandos pause resume y disconnect.

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -5,9 +5,10 @@ module.exports = {
   description: "desconecta la radio",
   execute(message, args) {
     const canalUsuario = message.member.voice.channel;
-    const canalBot = message.guild.voice.channel;
-    if (canalUsuario) {
-      if (canalUsuario === canalBot) {
+    const vozBot = message.guild.voice;
+    console.log(`${message} ${message.guild} ${message.guild.voice}`.red);
+    if (vozBot){
+      if (canalUsuario === vozBot.channel) {
         canalUsuario.leave();
         message.channel.send(
           `<@${message.author.id}> has apagado la XukysFiesta :(`
@@ -18,8 +19,8 @@ module.exports = {
             ` Por: ${message.author.username} <${message.author.id}>`.underline
               .bold
         );
-      } else if (!canalBot) message.reply("**¡El bot no está conectado!**");
-      else message.reply("**¡No estás en el mismo canal que el bot!**");
+      } else  message.reply("**¡No estás en el mismo canal que el bot!**");
     }
+    else message.reply("**¡El bot no está conectado!**"); 
   }
 };
--- commands/pause.js
+++ commands/pause.js
@@ -5,12 +5,14 @@ module.exports = {
     const canalUsuario = message.member.voice.channel;
     const canalBot = message.guild.voice.channel;
     const conexion = message.guild.voice.connection;
-
+if (canalUsuario){
     if (canalBot === canalUsuario) {
       const dispatcher = conexion.dispatcher;
       dispatcher.pause(true);
+      //falta poner mensaje de que se ha pausado.
     } else if (!canalBot)
-      message.channel.send("¡El bot no está en ningun canal!");
-    else message.reply("¡No estás en el mismo canal que el bot!");
+      message.channel.send("**¡El bot no está en ningun canal!**");
+   }
+   else message.reply("**¡No estás en el mismo canal que el bot!**");
   }
 };

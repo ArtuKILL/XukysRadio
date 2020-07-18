58712dd535aceeba63adaf0e8031d0fdcb8c5a86
ArtuKILL <39144443+ArtuKILL@users.noreply.github.com>
2020-07-18 18:37:49 -0400

    se quito el monitor de ping pq no funciona para glitch

--- index.js
+++ index.js
@@ -7,18 +7,8 @@ const color = require("colors");
 const fs = require("fs");
 const { codePointAt } = require('ffmpeg-static');
 const http = require('http');
-const express = require('express');
-const app = express();
 
-//PING
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
+
 
 client.commands = new Discord.Collection();
 const commandFiles = fs
